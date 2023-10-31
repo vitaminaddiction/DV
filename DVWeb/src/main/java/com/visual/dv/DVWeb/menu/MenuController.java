12844b182b7ef8e020358031843be3458da10ff8
vitaminaddiction <leehaejun23@naver.com>
2023-10-31 10:19:34 +0900

    DVweb 초기화

---
+++ DVWeb/src/main/java/com/visual/dv/DVWeb/menu/MenuController.java
@@ -0,0 +1,26 @@
+package com.visual.dv.DVWeb.menu;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @GetMapping("menu1")
+    public String menu1(){
+        return "menu/menu1";
+    }
+
+    @GetMapping("menu2")
+    public String menu2(){
+        return "menu/menu2";
+    }
+
+    @GetMapping("menu3")
+    public String menu3(){
+        return "menu/menu3";
+    }
+}
