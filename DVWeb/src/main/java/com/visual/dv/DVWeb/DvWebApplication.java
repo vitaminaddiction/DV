b618f88fe557c822bc26bd939f965647ceccaaa9
vitaminaddiction <leehaejun23@naver.com>
2023-10-31 09:22:29 +0900

    초기화

---
+++ DVWeb/src/main/java/com/visual/dv/DVWeb/DvWebApplication.java
@@ -0,0 +1,13 @@
+package com.visual.dv.DVWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvWebApplication.class, args);
+	}
+
+}
---
+++ DVWeb/src/test/java/com/visual/dv/DVWeb/DvWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visual.dv.DVWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
