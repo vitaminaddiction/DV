1fbf625e5796067d94083c2adaa5984cbce6a222
vitaminaddiction <leehaejun23@naver.com>
2023-11-03 14:28:43 +0900

    찐찐막 Menu1

--- DataVisualization/Server.py
+++ DataVisualization/Server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
-from io import BytesIO
-import temp1
+import Chart1
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +16,7 @@ def index():
         num = int(param['num'])
         num = num + 1
 
-    img_base64, value1, value2 = temp1.graph(firstDate, secondDate, item)
+    img_base64, value1, value2 = Chart1.graph(firstDate, secondDate, item)
     valList = [value1, value2]
     response_data = {
         "image": img_base64,
@@ -29,13 +28,11 @@ def index():
 @app.route("/menu1", methods=['POST'])
 def menu1():
     param = request.get_json()
-    if param and 'firstDate' in param and 'secondDate' in param and 'item' in param:
-        firstDate = str(param['firstDate'])
-        secondDate = str(param['secondDate'])
-        item = str(param['item'])
-        img_base64, value1, value2 = temp1.graph(firstDate, secondDate, item)
-    else:
-        img_base64, value1, value2 = temp1.graph()
+
+    firstDate = str(param['firstDate'])
+    secondDate = str(param['secondDate'])
+    item = str(param['item'])
+    img_base64, value1, value2 = Chart1.graph(firstDate, secondDate, item)
 
     valList = [value1, value2]
     response_data = {
@@ -47,7 +44,7 @@ def menu1():
 
 @app.route("/menu1/default", methods=['GET'])
 def menu1Default():
-    img_buffer = temp1.graphDefault()
+    img_buffer = Chart1.graphDefault()
 
     return Response(img_buffer, content_type='image/png')
 
