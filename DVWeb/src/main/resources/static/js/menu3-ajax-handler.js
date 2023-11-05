059c2f066fe9a0e1adbcbf8396603fe56ec8a68f
viataminaddiction <leehaejun23@naver.com>
2023-11-05 22:17:09 +0900

    menu1~4 수정 Chart.js 기본값 수정

---
+++ DVWeb/src/main/resources/static/js/menu3-ajax-handler.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    function fetchDataAndDrawChart() {
+        let firstDate = $("#firstDate").val();
+        let secondDate = $("#secondDate").val();
+        let item = $("#item").val();
+
+        $.ajax({
+            url: 'http://192.168.0.32:5000/menu3',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                firstDate,
+                secondDate,
+                item
+            }),
+            success: function(response) {
+                let imgSrc = 'data:image/png;base64,' + response.image;
+                $("#image").attr("src", imgSrc);
+                myBarChart.data.datasets[0].data = response.valList;
+                myBarChart.data.labels = [firstDate, secondDate];
+                myBarChart.data.datasets[0].label = item;
+                myBarChart.update();
+            }
+        });
+    }
+
+
+    $("#graphBtn").click( () => {
+        fetchDataAndDrawChart();
+    });
+});
